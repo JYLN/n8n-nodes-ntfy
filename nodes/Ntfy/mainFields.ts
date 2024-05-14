2eb24d960e79614a3709223176a4b66bf0b43930
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-05-14 09:21:19 -0600

    :writing_hand: Refactor field props order

--- nodes/Ntfy/mainFields.ts
+++ nodes/Ntfy/mainFields.ts
@@ -4,16 +4,17 @@ export const mainFields: INodeProperties[] = [
 	{
 		displayName: 'Use Custom Server',
 		name: 'useCustomServer',
+		description: 'Whether to use a custom server. If not, the default server will be used.',
 		type: 'boolean',
 		default: false,
 	},
 	{
 		displayName: 'Server URL',
 		name: 'serverUrl',
+		description: 'The URL of the ntfy.sh server',
 		type: 'string',
 		default: '',
 		placeholder: 'https://ntfy.sh',
-		description: 'The URL of the ntfy.sh server',
 		displayOptions: {
 			show: {
 				useCustomServer: [true],
